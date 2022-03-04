96db7e77c06deb43458fadc23c3d26f7d9039442
azhim <ariefazhim@gmail.com>
2022-03-04 16:07:02 +0800

    added code for intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import './Intro.scss';
+import React from "react";
+import "./Intro.scss";
 
 export default function Intro() {
   return (
-    <div className='intro' id='intro'>intro</div>
-  )
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there I'm</h2>
+          <h1>Azhim Arief</h1>
+          <h3>
+            Software <span></span>
+          </h3>
+        </div>
+        {/* <a href="#portfolio"> have some size problem cannot change
+          <img src="assets/down.png" alt="" />
+        </a> */}
+      </div>
+    </div>
+  );
 }
