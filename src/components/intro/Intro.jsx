4243ef4415e5d691703a465e715aee735673bfcc
azhim <ariefazhim@gmail.com>
2022-03-05 10:59:59 +0800

    Finished intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { init } from "ityped";
 import "./Intro.scss";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Engineer", "Designer"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +25,7 @@ export default function Intro() {
           <h2>Hi there I'm</h2>
           <h1>Azhim Arief</h1>
           <h3>
-            Software <span></span>
+            Software <span ref={textRef}></span>
           </h3>
           <a href="#portfolio">
             <img src="assets/down.png" alt="" />
